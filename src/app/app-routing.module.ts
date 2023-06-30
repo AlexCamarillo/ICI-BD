e2421ddb288de7cef1d815f9c97a09163c7f2a3c
Alex Camarillo <alex7camarillo@gmail.com>
2023-06-29 22:09:18 -0600

    Proyecto ICI en Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TablaComponent } from './tabla/tabla.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'tabla', component: TablaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
